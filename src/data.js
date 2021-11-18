d561c65b679c75f807e8ad9512f215d78cfba907
cdubyoo <chungwongg@gmail.com>
2021-11-18 13:36:54 -0800

    updated info

--- src/data.js
+++ src/data.js
@@ -2,4 +2,4 @@ export const classes = ['Class', 'SI', 'SE', 'HS', 'HB', 'DPS', 'SED']
 
 export const sedOrder = 'SED ORDER: Shad > NL > NW > WA > BM > MM > Sair > Bucc/TB > Hero/DW/Pala > Aran > Drk > Mages > Bishop'
 
-export const killOrder = 'KILL ORDER: Left/Leg > Right Arm/Wings(Leave at 30%) > Heads > Right Arm > Left Arm (don"t let other arm fall below 30%)'
+export const killOrder = 'KILL ORDER: Left(Leave above 65%)/Leg > Tail > Right Arm(Leave above 45%)/Wings(Leave at 30%) > Heads > Right Arm > Left Arm (don"t let other arm fall below 30% or sed)'
